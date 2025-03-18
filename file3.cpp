ce55e2125eb69e3782db8093094153b9e98be800
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-03-18 20:42:21 +0700

    Program SwitchCase

---
+++ file3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+float luasPersegiPanjang(float x, float y){
+    return x * y;
+}
+
+float luasPersegiPanjang(float x){
+    return 3.14 * x * x;
+}
+
+int main()
+{
+   
+}
