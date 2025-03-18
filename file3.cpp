b69a0624c1d1c5443d802b1c5c24d81b1b70d752
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-03-18 20:53:11 +0700

    membuat hitungan luas

--- file3.cpp
+++ file3.cpp
@@ -5,11 +5,20 @@ float luasPersegiPanjang(float x, float y){
     return x * y;
 }
 
-float luasPersegiPanjang(float x){
+float
+ luasPersegiPanjang(float x){
     return 3.14 * x * x;
 }
 
 int main()
 {
-   
-}
+    int pilihan;
+    float panjang,lebar,jejari;
+    do{
+        cout << "===Menu===" << endl;
+        cout << "1. Menghitung Luas Persegi Panjang" << endl;
+        cout << "2. Menghitung Luas Lingkaran" << endl;
+        cout << "3. Exit" << endl;
+        cin >> pilihan;
+
+        
